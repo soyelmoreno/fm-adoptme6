b72b09b6e3d957a05bb4873c95e489ff98965769
Carlos Moreno <soyelmoreno@gmail.com>
2021-06-16 23:19:54 -0700

    Initial commit of Adopt Me petfinder app.

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
+/* could do this, but don't need to: global React ReactDOM */
+
+/* 
+Tools in this course:
+- Prettier. npm i -D prettier. New file: /.prettierrc with just {}. Could also
+  add this to scripts in package.json: "format": "prettier --write \"src/**
+  /*.{js,jsx}\"" but the Prettier vscode extension is autoformatting on save, so
+  no need. Go to settings (CTRL + ,) and make sure `format on save` and
+  `Prettier: Require Config` are both checked. 
+
+- ESLint. npm install -D eslint eslint-config-prettier Unopinionated version of
+  ESLint. Config is in /.eslintrc.json. And add "lint" to scripts in
+  package.json. Install ESLint vscode extension.
+
+- Git. git init.
+
+- Parcel. npm i -D parcel. In scripts: "dev": "parcel src/index.html"
+
+- Can now delete unpkg.com script tags from index.html. Instead load react and
+react-dom from npm like this: npm install react react-dom, and then import
+them.
+
+- Now that we have the Parcel bundler, can do npm run dev to start a local
+  server. And move Pet into it's own file, then import it.
+
+- Babel. Transpiler. (With Parcel 2 you don't have to do this.) Add /.babelrc
+  with some config that allows us to use the most recent version of JSX. Install
+  like this: npm install -D @babel/core @babel/preset-react
+
+- Add browserslist to package.json. Check strings on browserslist.dev. Using
+  'defaults' covers 92% of users. 
+*/
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, {
+      name: "Sudo",
+      animal: "Dog",
+      breed: "Wheaten Terrier",
+    }),
+  ]);
+};
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
