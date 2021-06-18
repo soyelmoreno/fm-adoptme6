840248b209307d818f27445a5ff91e80bae526fc
Carlos Moreno <soyelmoreno@gmail.com>
2021-06-17 17:42:14 -0700

    Remove Pet. Create SearchParams component. Use a useState hook for location state.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 /* could do this, but don't need to: global React ReactDOM */
 
 /* 
@@ -45,9 +45,7 @@ const App = () => {
   return (
     <div>
       <h1>Adopt Me ---</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Sudo" animal="Dog" breed="Wheaten Terrier" />
+      <SearchParams />
     </div>
   );
 };
