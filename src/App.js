0166621e94a688ebaf2f0c9a616ea9f05f16119e
Carlos Moreno <soyelmoreno@gmail.com>
2021-06-17 17:16:07 -0700

    Rewrite Pet and App using JSX. Remove unneeded React import (Babel does it for us)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
 /* could do this, but don't need to: global React ReactDOM */
@@ -33,24 +32,16 @@ them.
 - Add browserslist to package.json. Check strings on browserslist.dev. Using
   'defaults' covers 92% of users. 
 */
+
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Sudo",
-      animal: "Dog",
-      breed: "Wheaten Terrier",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me ---</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Sudo" animal="Dog" breed="Wheaten Terrier" />
+    </div>
+  );
 };
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+
+ReactDOM.render(<App />, document.getElementById("root"));
