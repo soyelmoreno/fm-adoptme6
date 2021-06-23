e8f9fafdf19cd0cef4a82777d1fa78000f7b6f12
Carlos Moreno <soyelmoreno@gmail.com>
2021-06-23 00:06:48 -0700

    Create and use Results component to display search results. Refactor Pet component

---
+++ src/Results.js
@@ -0,0 +1,24 @@
+import Pet from "./Pet";
+
+const Results = ({ pets }) => {
+  return (
+    <div className="search">
+      {!pets.length ? (
+        <h2>No pets found</h2>
+      ) : (
+        pets.map((pet) => (
+          <Pet
+            key={pet.id}
+            animal={pet.animal}
+            name={pet.name}
+            breed={pet.breed}
+            images={pet.images}
+            location={`${pet.city}, ${pet.state}`}
+            id={pet.id}
+          />
+        ))
+      )}
+    </div>
+  );
+};
+export default Results;
